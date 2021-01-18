6b22073e51527e508c1046712deed15aaa8fa94f
edcamargo <edwin.desenv@gmail.com>
2021-01-17 21:59:42 -0300

    Implementação dos atributos dos Dtos

--- api/src/main/kotlin/com/backend/api/dtos/FuncionarioDto.kt
+++ api/src/main/kotlin/com/backend/api/dtos/FuncionarioDto.kt
@@ -1,4 +1,15 @@
 package com.backend.api.dtos
 
-class FuncionarioDto {
-}
+//import org.hibernate.validator.constraints.NotEmpty
+
+data class FuncionarioDto (
+    //@get:NotEmpty(message = "Nome não pode ser vazio.")
+    //@get:Length()
+    val nome: String = "",
+    val email: String = "",
+    val senha: String? = null,
+    val valorHora: String? = null,
+    val qtdHorasTrabalhoDia: String? = null,
+    val qtdHorasAlmoco: String? = null,
+    val id: String? = null
+)
--- api/src/main/kotlin/com/backend/api/dtos/LancamentoDto.kt
+++ api/src/main/kotlin/com/backend/api/dtos/LancamentoDto.kt
@@ -1,4 +1,13 @@
 package com.backend.api.dtos
 
-class LancamentoDto {
-}
+data class LancamentoDto (
+    //@get:NotEmpty(message = "Data não pode ser vazia.")
+    val data: String? = null,
+
+    //@get:NotEmpty(message = "Data não pode ser vazia.")
+    val tipo: String? = null,
+    val descricao: String? = null,
+    val localizacao: String? = null,
+    val funcionarioId: String? = null,
+    val id: String? = null
+)
